03c3096fa396caf684c7312ed371e1132f42305a
Mark Nevison <mnevison2@gmail.com>
2024-10-03 01:50:42 +0000

    added: lots more

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -9,3 +9,27 @@ document.addEventListener("DOMContentLoaded", function () {
   let modals = document.querySelectorAll(".modal");
   M.Modal.init(modals, {});
 });
+
+// date picker
+
+document.addEventListener("DOMContentLoaded", function () {
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" },
+  });
+});
+
+// select initilization
+
+document.addEventListener("DOMContentLoaded", function () {
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+});
+
+// collapsable
+
+document.addEventListener("DOMContentLoaded", function () {
+  let collapsibles = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(collapsibles);
+});
