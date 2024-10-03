70ad06bc5d545d1f118e5591ba780cb35449df45
Mark Nevison <mnevison2@gmail.com>
2024-10-03 00:51:40 +0000

    added:alot of stuff

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+});
+
+// Modal
+
+document.addEventListener("DOMContentLoaded", function () {
+  let modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals, {});
+});
